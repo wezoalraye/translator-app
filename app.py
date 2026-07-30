f495e5ea5e93d7e787d41ba726e97d55e980f932
Mohamed Wael <159113652+wezoalraye@users.noreply.github.com>
2026-07-30 17:15:55 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from transformers import pipeline
+
+@st.cache_resource
+def load_model():
+    return pipeline(
+        "translation",
+        model="mwael399/arabic-english-translator"
+    )
+
+translator = load_model()
+
+st.title("Arabic → English Translator")
+
+text = st.text_area("Enter Arabic text")
+
+if st.button("Translate"):
+    result = translator(text)
+    st.success(result[0]["translation_text"])
